6aca2a77b9561a5f92506114f11db66c5a05f8df
PKTOSE <jungwon0719a@naver.com>
2020-10-27 23:09:02 +0900

    2020 10 27

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -18,18 +18,23 @@ const Home = ({ userObj }) => {
       });
   }, []);
   return (
-    <div className="container">
-      <NweetFactory userObj={userObj} />
-      <div style={{ marginTop: 30 }}>
-        {nweets.map((nweet) => (
-          <Nweet
-            key={nweet.id}
-            nweetObj={nweet}
-            isOwner={nweet.creatorId === userObj.uid}
-          />
-        ))}
+    <>
+      <div>
+        <h1>Hi</h1>
       </div>
-    </div>
+      <div className="container">
+        <NweetFactory userObj={userObj} />
+        <div style={{ marginTop: 30 }}>
+          {nweets.map((nweet) => (
+            <Nweet
+              key={nweet.id}
+              nweetObj={nweet}
+              isOwner={nweet.creatorId === userObj.uid}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 export default Home;
